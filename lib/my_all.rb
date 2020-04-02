3e579724cf9406d99640fc6af6c1f9b88fd1b146
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-02 21:22:30 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   block_retun_values = []
   while i < collection.length
-    yield(collection[i])
+    block_retun_values << yield(collection[i])
     i = i + 1
   end
 end
